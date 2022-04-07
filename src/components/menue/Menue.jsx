81e2b8616948f53ac8ee2f141c0d4668965476a2
omarelkamah <omarwork010@gmail.com>
2022-04-07 09:51:33 -0700

    Fix Navbar

--- src/components/menue/Menue.jsx
+++ src/components/menue/Menue.jsx
@@ -1,31 +1,25 @@
-import React from 'react';
-import Togler from '../togler/Togler';
+import React from 'react'
+import Togler from '../togler/Togler'
 
-import './menue.scss';
+import './menue.scss'
 
-export default function Menue({openMenue, handeleMenue, dark, setDark}) {
+export default function Menue ({ openMenue, handeleMenue, dark, setDark }) {
   return (
-    <div className={'menue ' + (openMenue && "active")}>
-        <ul className="list">
-            <li onClick={() => handeleMenue(!openMenue)}>
-                <a href="#home">Home</a>
-            </li>
-            <li onClick={() => handeleMenue(false)}>
-                <a href="#protofolio">Protofolio</a>
-            </li>
-            <li onClick={() => handeleMenue(false)}>
-                <a href="#works">Works</a>
-            </li>
-            <li onClick={() => handeleMenue(false)}>
-                <a href="#testimoials">Testimonials</a>
-            </li>
-            <li onClick={() => handeleMenue(false)}>
-                <a href="#contact">Contact</a>
-            </li>
-            <li>
-                <Togler dark={dark} setDark={setDark} />
-            </li>
-        </ul>
+    <div className={'menue ' + (openMenue && 'active')}>
+      <ul className='list'>
+        <li onClick={() => handeleMenue(!openMenue)}>
+          <a href='#home'>Home</a>
+        </li>
+        <li onClick={() => handeleMenue(false)}>
+          <a href='#protofolio'>Protofolio</a>
+        </li>
+        <li onClick={() => handeleMenue(false)}>
+          <a href='#testimoials'>Testimonials</a>
+        </li>
+        <li>
+          <Togler dark={dark} setDark={setDark} />
+        </li>
+      </ul>
     </div>
-  );
+  )
 }
