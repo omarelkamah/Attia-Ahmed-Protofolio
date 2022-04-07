059e51839a55ff156f553476c4fb36407fb8f3be
omarelkamah <omarwork010@gmail.com>
2022-04-07 09:08:30 -0700

    Change Photos to increase performance

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -9,67 +9,67 @@ export default function Testimonials ({ dark }) {
   const data = [
     {
       id: 1,
-      img: 'assets/testiImg/1.png'
+      img: 'assets/testiImg/1.jpg'
     },
     {
       id: 2,
-      img: 'assets/testiImg/2.png'
+      img: 'assets/testiImg/2.jpg'
     },
     {
       id: 3,
-      img: 'assets/testiImg/3.png'
+      img: 'assets/testiImg/3.jpg'
     },
     {
       id: 4,
-      img: 'assets/testiImg/4.png'
+      img: 'assets/testiImg/4.jpg'
     },
     {
       id: 5,
-      img: 'assets/testiImg/5.png'
+      img: 'assets/testiImg/5.jpg'
     },
     {
       id: 6,
-      img: 'assets/testiImg/6.png'
+      img: 'assets/testiImg/6.jpg'
     },
     {
       id: 7,
-      img: 'assets/testiImg/7.png'
+      img: 'assets/testiImg/7.jpg'
     },
     {
       id: 8,
-      img: 'assets/testiImg/8.png'
+      img: 'assets/testiImg/8.jpg'
     },
     {
       id: 9,
-      img: 'assets/testiImg/9.png'
+      img: 'assets/testiImg/9.jpg'
     },
     {
       id: 10,
-      img: 'assets/testiImg/10.png'
+      img: 'assets/testiImg/10.jpg'
     },
     {
       id: 11,
-      img: 'assets/testiImg/11.png'
+      img: 'assets/testiImg/11.jpg'
     },
     {
       id: 12,
-      img: 'assets/testiImg/12.png'
+      img: 'assets/testiImg/12.jpg'
     },
     {
       id: 13,
-      img: 'assets/testiImg/13.png'
+      img: 'assets/testiImg/13.jpg'
     },
     {
       id: 14,
-      img: 'assets/testiImg/14.png'
+      img: 'assets/testiImg/14.jpg'
     },
     {
       id: 15,
-      img: 'assets/testiImg/15.png'
+      img: 'assets/testiImg/15.jpg'
     },
     {
       id: 16,
-      img: 'assets/testiImg/16.png'
+      img: 'assets/testiImg/16.jpg'
     }
   ]
 
